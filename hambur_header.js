70e9d3f227fd8c6a6f63d970aec8b0afcfe6165e
nezzo <lestatx@yandex.ru>
2016-10-14 01:25:33 +0300

    13.10.2016

--- hambur_header.js
+++ hambur_header.js
@@ -1,47 +1,48 @@
   $(document).ready(function(){
-    alert("111");
     
-  $( "cross_top" ).hide();
+   $( ".cross_top" ).hide();
   //$( ".menu-top" ).hide();
   $( ".hamburger_top" ).click(function() {
-  $( ".menu-top" ).slideToggle( "slow", function() {
+  $( ".top-menu" ).slideToggle( "slow", function() {
   $( ".hamburger_top" ).hide();
+  $( ".auth" ).show(); 
+  $( ".top-menu" ).show();  
+  $( ".cross_top" ).show();
+  $( ".cross_top" ).show();
+  });
+  });
+  
+  $( ".cross_top" ).click(function() {
+  $( ".top-menu" ).slideToggle( "slow", function() {
+  $( ".cross_top" ).hide();
+  $( ".hamburger_top" ).show();
   $( ".auth" ).hide();
   $( ".top-menu" ).hide(); 
   });
-
-  $( "cross_top" ).click(function() {
-  $( ".menu-top" ).slideToggle( "slow", function() {
-  $( "cross_top" ).hide();
-  $( ".hamburger_top" ).show();
-  $( ".auth" ).show();
-  $( ".top-menu" ).show(); 
-  });
   });
   
   
-   $( "cross_menu" ).hide();
+   $( ".cross_menu" ).hide();
   //$( ".menu-top" ).hide();
-  $( ".cross_menu" ).click(function() {
-  $( ".menu-top" ).slideToggle( "slow", function() {
-  $( ".cross_menu" ).hide();
-  $( ".auth" ).hide();
-  $( ".cat-menu ul" ).hide(); 
-  });
-
-  $( "cross_top" ).click(function() {
-  $( ".menu-top" ).slideToggle( "slow", function() {
-  $( "cross_top" ).hide();
+  $( ".hamburger_menu" ).click(function() {
+  $( ".cat-menu ul" ).slideToggle( "slow", function() {
+  $( ".hamburger_menu" ).hide();
   $( ".cross_menu" ).show();
-  $( ".auth" ).show();
-  $( ".cat-menu ul" ).show(); 
+  });
+ });
+  
+  $( ".cross_menu" ).click(function() {
+  $( ".cat-menu ul" ).slideToggle( "slow", function() {
+  $( ".cross_top" ).hide();
+  $( ".cross_menu" ).hide()
+  $( ".hamburger_menu" ).show();
   });
   });
+});  
   
   
   
-  
-  });
+
     
   
     
